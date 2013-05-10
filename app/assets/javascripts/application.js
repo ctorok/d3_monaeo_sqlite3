d78fece7295c118d6dd019100522e3f92883f0f9
ctorok <ctorok@gmail.com>
2013-05-10 14:35:07 -0400

    yay! loaded google map with coffeescript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require 'underscore.js'
-//= require 'moment.js'
 //= require 'd3.js'
-//=# require 'arc.js'
-//=# require 'circle.js'
-//=# require 'circlejson.js'
 //=# require 'g_map.js'
-//= require 'join.js'
+//= require 'g_mapcoffe.js'
+
